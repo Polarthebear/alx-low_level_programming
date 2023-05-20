f0e5ac87ded635363249bc56a690b0266e89371a
Polarthebear <thatotae325@gmail.com>
2023-05-20 05:02:14 -0700

    Program assigns a random number to variable n, and prints out a result

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,19 +2,18 @@
 #include <time.h>
 #include <stdio.h>
 
-/* *
+/**
  * betty stle doc
  * main - main function
- * program will get a random number assigned to the n variable and print the number telling the user if it is positive, negative or zero
- * Return 0 (success)
- * */
+ * program: assign a random number to the n variable and print the number
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/*Code goes here*/
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
