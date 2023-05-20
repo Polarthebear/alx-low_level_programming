709c5490e0df547995a080943c1cd0a6617fa4da
Polarthebear <thatotae325@gmail.com>
2023-05-20 06:54:36 -0700

    Program prints the last digit of the n variable followed by weather it is greater than 5 or not

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: print the last digit of the number stored in the variable n.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	int i;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (i > 5)
+	i{
+		printf("Last digit of %d is %d and is greater than 5\n", n, i);
+	}
+	else if (i = !0 && i < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, i);
+	}
+	return (0);
+}
