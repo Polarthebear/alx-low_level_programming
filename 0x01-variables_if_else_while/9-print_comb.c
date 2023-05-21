3606adb446f4c1b2f6b34e1ae6a35bc075951f18
Polarthebear <thatotae325@gmail.com>
2023-05-21 06:22:26 -0700

    Program that prints all single digits followed by a , and space.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,11 @@ int main(void)
 	while (num <= '9')
 	{
 		putchar(num);
-		putchar(',');
-		putchar(' ');
+		if(num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		num++;
 	}
 	putchar('\n');
