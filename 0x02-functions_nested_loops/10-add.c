af7a07408be2522b27d6926f3bf6c5a75c44a592
Polarthebear <thatotae325@gmail.com>
2023-05-24 11:59:28 -0700

    Program that adds 2 ints and prints a result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add- main entry point
+ * @a: int to be checked
+ * @b: int to be checked
+ * Description: Function that adds 2 ints and returns the value
+ * Return: sum of 2 ints
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
