650479129d47c7a78443fb814d8d2d1a2048bee6
Polarthebear <thatotae325@gmail.com>
2023-05-25 14:31:02 -0700

    Trial 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - main entry point
+ * @n: = print_line
+ * Description: print '_' for each number of n
+ */
+void print_line(int n)
+{
+	char print_line = 'n';
+
+	while (print_line == 'n')
+	{
+		if (print_line != 0 || print_line > 0)
+		{
+			_putchar("_");
+		}
+		_putchar("\n");
+	}
+	_putchar("\n");
+}
