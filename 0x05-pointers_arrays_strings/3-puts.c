525e6862f6a3c592551c729605651c6694f65464
Polarthebear <thatotae325@gmail.com>
2023-05-30 12:27:47 -0700

    Function that prints out a string to stdout.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - main entry point
+ * @str: string to be printed out
+ * Description: Function that prints a string to stdout
+ */
+void _puts(char *str)
+{
+	char *str;
+
+	_puts(*str);
+}
