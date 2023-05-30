91ae1efc0e9419decbe828dcaeeaf006255319b6
Polarthebear <thatotae325@gmail.com>
2023-05-30 04:41:16 -0700

    Function that updates the value to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - main entry point
+ * @n: int to be updated
+ * Description: Function takes a pointer to an int and updates value to 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
