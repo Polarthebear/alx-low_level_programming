5ae9862a1b2750287645a77994873674f9cee8a1
Polarthebear <thatotae325@gmail.com>
2023-06-05 12:15:59 -0700

    Advanced Tasks: 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - Main entry point
+ * @s: pointer to a pointer
+ * @to: pointer to a char
+ * Description: Sets value to a pointer of a char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
