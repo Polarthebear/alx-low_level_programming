08528a5f63bca227cfdb81cc1c305bad3f4b0655
Polarthebear <thatotae325@gmail.com>
2023-06-12 09:16:44 -0700

    Function that prints all arguments recieved.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ * @argc: command line argument
+ * @argv: argument vector
+ * Description: Print all the arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	for (int i = 0, i < argc, i++)
+	{
+		printf("s%\n", argv[i]);
+	}
+	return (0);
+}
