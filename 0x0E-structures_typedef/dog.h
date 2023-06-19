a293441ef7cf86847fe2fc8d10952f9b6601999d
Polarthebear <thatotae325@gmail.com>
2023-06-19 14:33:01 -0700

    New type definition: struct dog.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - New type 
+ * @name: name of dog
+ * @owner: name of owner
+ * @age: age of dog
+ * Description: new type definition
+ */
+struct dog 
+{
+	char *name;
+	char *owner;
+	float age;
+};
