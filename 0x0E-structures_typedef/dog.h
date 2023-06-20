e32ba2b943e9da2b16a3dfb51f66e65cf0920181
Polarthebear <thatotae325@gmail.com>
2023-06-20 14:44:06 -0700

    Updated header file.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,15 +15,17 @@ struct dog
 	float age;
 };
 
+/**
+ * dog_t - new typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-/**
- * dog_t - New typedef for struct dog
- */
-typedef struct dog dog_t;
 
 
 #endif
