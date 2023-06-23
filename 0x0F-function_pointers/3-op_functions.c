f1c5be1e67feb609b6519fb0c71a15f1b0807ed9
Polarthebear <thatotae325@gmail.com>
2023-06-23 10:25:22 -0700

    a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - main entry point
+ * @a: int #1
+ * @b: int #2
+ * Description: add two integers
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - main entry point
+ * @a: int #1
+ * @b: int #2
+ * Description: subtract two integers
+ * Return: difference between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - main entry point
+ * @a: int #1
+ * @b: int #2
+ * Description: multiplies two integers
+ * Return: Product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - main entry point
+ * @a: int #1
+ * @b: int #2
+ * Description: divedes two ints
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - main entry point
+ * @a: int #1
+ * @b: int #2
+ * Description: remainder of division of a and b
+ * Return: remainder of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
