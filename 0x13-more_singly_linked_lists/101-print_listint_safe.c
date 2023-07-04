54b11eeeaaf85c864b384337a55992b8b8635fce
Polarthebear <thatotae325@gmail.com>
2023-07-04 16:53:07 -0700

    Advanced Tasks

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,13 @@
+#include <stdio.>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * looped_listint_len - Main entry point
+ * Description: unique nodes are counted
+ * @head: pointer
+ * Return: 0
+ */
+size_t print_listint_safe(const listint_t *head);
+{
+
