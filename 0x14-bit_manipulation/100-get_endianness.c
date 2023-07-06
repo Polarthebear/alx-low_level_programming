2781df50878093fd18cb250d79f8529739125096
Polarthebear <thatotae325@gmail.com>
2023-07-06 15:42:45 -0700

    Advanced Task: 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,7 +5,7 @@
  * Description: check if the machine is a big or little endian
  * Return: 1 (little) or 0 (big)
  */
-int get_endianess(void)
+int get_endianness(void)
 {
 	unsigned int n = 1;
 	char *c = (char *) &n;
