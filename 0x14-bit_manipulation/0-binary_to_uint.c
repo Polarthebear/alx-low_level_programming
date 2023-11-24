d7f17e6cb471d057e3be777b693e1111594e7701
Polarthebear <thatotae325@gmail.com>
2023-11-24 07:01:09 -0800

    Function that converts a binary number to an unsigned int.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "bitman.h"
+
+/**
+ * binary_to_uint - Covert number to unsigned int from binary
+ * @b: string with binary num
+ * return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int deci = 0;
+	int i = 0;
+
+	if (!b)
+	{
+		retrun (0);
+	}
+	for (i; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+		{
+			return (0);
+		}
+		deci = 2 * deci + (b[i] - '0');
+	}
+
+	return (deci);
+}
