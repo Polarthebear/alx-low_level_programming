5613e6d41a039304b01ab6faf305436b94ef5532
Polarthebear <thatotae325@gmail.com>
2023-11-24 07:35:09 -0800

    Function that prints the binary representation of a number.

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <unistd.h>
-
+/**
+ * _putchar - write to standar output
+ * @c: char to print
+ * Return: 1 (Success) or -1 (Fail)
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
