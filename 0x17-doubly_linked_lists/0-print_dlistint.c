a6c9162e0a2814225baec70090660841e896887d
Polarthebear <thatotae325@gmail.com>
2024-01-11 11:57:55 -0800

    Function that prints all the elements of a dlistint_t list.

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,9 +2,9 @@
 
 /**
  * print_dlistint - main entry point
- * Description: Function that prints all the 
+ * Description: Function that prints all the
  * elemnts of a dlistint_t list
- * h: head
+ * @h: head
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
