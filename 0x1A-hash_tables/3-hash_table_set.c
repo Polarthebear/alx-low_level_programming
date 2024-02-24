c174235f69c68bd286056374d4072fe4a4f1cf62
Polarthebear <thatotae325@gmail.com>
2024-02-24 03:21:37 -0800

    Function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(ht->array[i]->value);
 			ht->array[i]->value = value_cpy;
-			return (i);
+			return (1);
 		}
 	}
 	new = malloc(sizeof(hash_node_t));
